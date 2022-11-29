55450ee281179b995e79cde55ffce49cf58ea2ec
Dennis Otieno <dennisotieno@Denniss-MacBook-Pro.local>
2022-11-30 00:16:40 +0300

    7-islower.py

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Function that checks for lowercase charactors
+
+def islower(charctor):
+    """Checks for lowercase charactors."""
+    if ord(charactor) >= 97 and ord(charactor) <= 122:
+        return True
+    else:
+        return False
