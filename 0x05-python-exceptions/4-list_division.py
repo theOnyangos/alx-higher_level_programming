ed95d77750415c00a8b8b24be0100cc15e0050f8
theOnyangos <otienodennis29@gmail.com>
2022-12-20 12:29:28 +0000

    small changes on both files

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 # 4-list_division.py
-# Brennan D Baraban <375@holbertonschool.com>
-
 
 def list_division(my_list_1, my_list_2, list_length):
     """Divides two lists element by element.
